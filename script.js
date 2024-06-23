04c899508e3fe95c4864e9d17b44f62710d546f8
Khaled Ballour <khaled-ballour@ballourk@gmail.com>
2024-06-23 11:26:25 +0300

    score feature done

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ const number = 20;
 
 const message = document.querySelector('.message');
 const guessEle = document.querySelector('.guess');
+const scoreEle = document.querySelector('.score');
+const highscoreEle = document.querySelector('.highscore');
 document.querySelector('.check').addEventListener('click', e => {
   const guess = Number(guessEle.value);
   if (!guess) message.textContent = 'No number!';
-  else
-    message.textContent =
-      guess === number ? 'Correct' : guess > number ? 'Too big' : 'Too small';
+  else {
+    if (guess === number) {
+      message.textContent = 'Correct guess';
+      if (Number(scoreEle.textContent) > Number(highscoreEle.textContent))
+        highscoreEle.textContent = scoreEle.textContent;
+      scoreEle.textContent = '20';
+    } else {
+      message.textContent = guess > number ? 'Too high' : 'Too small';
+      scoreEle.textContent = Number(scoreEle.textContent) - 1;
+    }
+  }
 });
