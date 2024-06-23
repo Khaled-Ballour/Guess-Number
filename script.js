07fdaa193cd71ae7679ff10fcb390dee8e608842
Khaled Ballour <khaled-ballour@ballourk@gmail.com>
2024-06-23 12:32:53 +0300

    fixing the round score

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 'use strict';
 
 const number = Math.trunc(Math.random() * 20) + 1;
+let withinTheRound = true;
 
 const message = document.querySelector('.message');
 const guessEle = document.querySelector('.guess');
 const scoreEle = document.querySelector('.score');
 const highscoreEle = document.querySelector('.highscore');
+
 document.querySelector('.check').addEventListener('click', e => {
+  if (!withinTheRound) return;
   const guess = Number(guessEle.value);
   if (!guess) message.textContent = 'No number!';
   else {
@@ -14,10 +17,11 @@ document.querySelector('.check').addEventListener('click', e => {
       message.textContent = 'Correct guess';
       if (Number(scoreEle.textContent) > Number(highscoreEle.textContent))
         highscoreEle.textContent = scoreEle.textContent;
-      scoreEle.textContent = '20';
+      withinTheRound = false;
     } else {
       message.textContent = guess > number ? 'Too high' : 'Too small';
-      scoreEle.textContent = Number(scoreEle.textContent) - 1;
+      if (Number(scoreEle.textContent) > 0)
+        scoreEle.textContent = Number(scoreEle.textContent) - 1;
     }
   }
 });
