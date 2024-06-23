7765c0c3e8a0f01438425cbe35abd2039e0f42b5
Khaled Ballour <khaled-ballour@ballourk@gmail.com>
2024-06-23 10:15:51 +0300

    script logic

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const number = 20;
+
+const message = document.querySelector('.message');
+document.querySelector('.check').addEventListener('click', e => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) message.textContent = 'No number!';
+  else
+    message.textContent =
+      guess === number ? 'Correct' : guess > number ? 'Too big' : 'Too small';
+});
